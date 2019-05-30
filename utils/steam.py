7835720720f42b131d24d84695e605e96150d464
Thobias N. Hejstvig-Larsen <thobias.nordgaard@gmail.com>
2019-05-30 20:05:26 +0200

    remove ping

--- utils/steam.py
+++ utils/steam.py
@@ -1,16 +1,6 @@
 import logging
 import traceback
-from aio_ping import ping
 from steam import game_servers as gs
-
-
-async def _ping_server(ip_address):
-    """
-    pings the given ip address
-    :param ip_address:
-    :return: float
-    """
-    return ping(ip_address, timeout=1000, count=1)
 
 
 async def get_server_info(address):
@@ -21,13 +11,9 @@ async def get_server_info(address):
     """
     try:
         result = gs.a2s_info(address)
-        ping = await _ping_server(address[0])
     except Exception as e:
         logging.error(traceback.format_exc())
         result = None
-        ping = None
 
-    if ping and result:
-        result['ping'] = ping
     return result
 
